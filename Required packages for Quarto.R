175a071b6e69c37acac87f11bdd52c057327f0c5
Dr. Nu Nu Khaing <43107631+Nunukhaing-3@users.noreply.github.com>
2023-07-07 21:48:23 +0630

    Add files via upload

---
+++ Required packages for Quarto.R
@@ -0,0 +1,11 @@
+
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("tinytex")
+install.packages("here")
+install.packages("tidyverse")
+install.packages("broom")
+install.packages("fs")
+install.packages("usethis")
+
+
